b8b7c287cd529a5225555ffb4ffed30cdb3fe668
imjae <code@kakao.com>
2022-06-05 16:33:16 +0900

    Duck 클래스에서 Fly, Quack 기능 제거

--- Assets/Scripts/Duck.cs
+++ Assets/Scripts/Duck.cs
@@ -2,9 +2,7 @@
 
 public abstract class Duck
 {
-    protected virtual void Quack(){}
     protected virtual void Swim(){}
-    protected virtual void Fly(){}
     protected virtual void Display(){}
 
 }
